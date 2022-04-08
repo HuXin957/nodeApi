de91c7705caf38fbc0533884719ff626120f22a1
huqiang <1095518902@qq.com>
2022-04-08 13:23:09 +0800

    fix:db

--- db/index.js
+++ db/index.js
@@ -1,22 +1,27 @@
-const mysql=require('mysql')
-module.exports={
-    config:{
-        host:'124.223.212.23',
-        port:3306,
-        user:'playwith',
-        password:'kJcTbxyFRwZkRLfz',
-        database:'playwith'
-    },
-    connection(sql,arr,callback){
-        const pool=mysql.createPool(this.config)
-        pool.getConnection((err,connection)=>{
-            if(!err){
-                connection.query(sql,arr,callback)
-                connection.release()
-            }else{
-                return err
-            }
-        })
-
-    }
-}
+const mysql = require("mysql");
+module.exports = {
+  config: {
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "123456",
+    database: "nodeback",
+    // host:'124.223.212.23',
+    // port:3306,
+    // user:'playwith',
+    // password:'kJcTbxyFRwZkRLfz',
+    // database:'playwith'
+  },
+  connection(sql, arr, callback) {
+    const pool = mysql.createPool(this.config);
+    pool.getConnection((err, connection) => {
+      if (!err) {
+        connection.query(sql, arr, callback);
+        connection.release();
+      } else {
+        console.log(err);
+        return err;
+      }
+    });
+  },
+};
--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,32 @@ const express = require("express");
 const db = require("../db");
 const router = express.Router();
 
-router.post("/leter", function (req, res, next) {
-  const sql = "delete * from order_taking where id=?";
-  const area = 5;
-  db.connection(sql, [area], (err, data) => {
+router.post("/createUser", function (req, res, next) {
+  const sql =
+    "insert into user_info (user_id, user_name, sex, create_date) values (?,?,?,?)";
+  const { user_name, sex } = req.body;
+  const create_date = Date.now();
+  const user_id = create_date;
+  db.connection(
+    sql,
+    [user_id, user_name, sex, create_date],
+    (err, data) => {
+      res.json({
+        code: 0,
+        msg: "提交成功",
+      });
+    }
+  );
+});
+
+router.get("/getUserList", function (req, res, next) {
+  const sql = "select * from user_info";
+
+  db.connection(sql, [], (err, data) => {
     res.json({
       code: 0,
-      msg: "提交成功",
+      msg: "查询成功",
+      list: data,
     });
   });
 });
